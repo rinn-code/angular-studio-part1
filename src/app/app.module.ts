436fa87af2c39ce584f6d4d1a1764590fba12395
Rinn Mattox <rinnreed@gmail.com>
2022-07-20 21:50:57 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent
   ],
   imports: [
     BrowserModule
